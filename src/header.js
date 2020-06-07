50557c3094b269d36915c923894d3f6edfb22792
Beeno Tung <aabbcc1241@yahoo.com.hk>
2020-06-08 02:18:23 +0800

    add matches for HN

--- src/header.js
+++ src/header.js
@@ -6,13 +6,13 @@
 // @author       Beeno Tung
 // @match        https://*
 // @match        http://*
+// @match        https://news.ycombinator.com/item?id=*
+// @match        https://news.ycombinator.com/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
   'use strict';
-
-  var parcelRequire,t,require,module,define
 
   // Your code here...
 })();
